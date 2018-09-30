2a5a078498ae53c38554041007f99b22997727fc
camiula <camilacarwalho3@gmail.com>
2018-09-30 14:28:55 -0300

    script

---
+++ Scripts/povoamentoPESSOA.sql
@@ -0,0 +1,11 @@
+-- Povoando a relação Pessoa
+INSERT INTO Pessoa VALUES(‘154.288.064-51’, ‘Teofanes Ferreira’,’99123456)’;
+INSERT INTO Pessoa VALUES(‘358.947.104-21’, ‘Milena Carvalho’,’98752374’;
+INSERT INTO Pessoa VALUES(‘354.514.294-99’, ‘Manoel de Sousa’,’93098725)’;
+INSERT INTO Pessoa VALUES(‘734.042.214-51’, ‘Amanda Ferreira’,’93827460)’;
+INSERT INTO Pessoa VALUES(‘559.629.914-60’, ‘Maria das Graças’,’99097162)’;
+INSERT INTO Pessoa VALUES(‘695.997.444-54’, ‘Antonia Lins’,’98152674)’;
+INSERT INTO Pessoa VALUES(‘690.418.134-62’, ‘Eduardo Martins’,’99523850)’;
+INSERT INTO Pessoa VALUES(‘846.101.944-06’, ‘Ana Isabel dos Santos’,’99825556)’;
+INSERT INTO Pessoa VALUES(‘146.287.484-38’, ‘Junior da Silva’,’99123499)’;
+INSERT INTO Pessoa VALUES(‘819.174.014-18’, ‘Ricardo Costa’,’99553400)’;
